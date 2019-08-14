cc9fc5a28eadb395ec44ed2a6919dbafe55364df
achenson <45999022+Achenson@users.noreply.github.com>
2019-08-14 15:12:40 +0200

    get single + test working (4 tests working)

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,23 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-var dotenv = require('dotenv');
-var mongoose = require('mongoose');
+var expect = require("chai").expect;
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectId;
+var dotenv = require("dotenv");
+var mongoose = require("mongoose");
 var shortid = require("shortid");
 
 //userId: {
-  //type: String,
-  //default: shortid.generate
+//type: String,
+//default: shortid.generate
 //},
 
 dotenv.config();
@@ -30,9 +30,7 @@ mongoose
   .then(() => console.log("connection succesfull"))
   .catch(err => console.log(err));
 
-
-module.exports = function (app) {
-
+module.exports = function(app) {
   const BookSchema = new mongoose.Schema({
     title: {
       type: String,
@@ -46,96 +44,82 @@ module.exports = function (app) {
       type: Array,
       default: []
     }
+  });
 
-  })
+  const Book = mongoose.model("Book", BookSchema);
 
-  const Book = mongoose.model('Book', BookSchema);
+  app
+    .route("/api/books")
+    .get(function(req, res) {
+      Book.find().exec((err, data) => {
+        if (err) return console.error(err);
 
+        let newData = [];
 
+        for (let el of data) {
+          let newArrayObj = {};
 
+          newArrayObj._id = el._id;
+          newArrayObj.title = el.title;
+          newArrayObj.commentcount = el.comments.length;
 
-
-
-
-  app.route('/api/books')
-    .get(function (req, res){
-
-
-     Book.find()
-      .exec(
-        (err, data) => {
-          if (err) return console.error(err);
-          
-          let newData = [];
-
-          for (let el of data) {
-            let newArrayObj = {};
-
-            newArrayObj._id = el._id;
-            newArrayObj.title = el.title;
-            newArrayObj.commentcount = el.comments.length;
-
-           newData.push(newArrayObj);
-
-      
-          }
-          
-
-          res.send(
-            
-            newData
-            
-          )
-
-
+          newData.push(newArrayObj);
         }
-      )
 
+        res.send(newData);
+      });
 
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
-    .post(function (req, res){
+
+    .post(function(req, res) {
       var title = req.body.title;
 
       let newBook = new Book({
         title: title
-
-      })
+      });
 
       newBook.save(err => {
-        if (err) return console.error(err)
-      })
+        if (err) return console.error(err);
+      });
 
       console.log(req.body);
       res.json(newBook);
-      
 
       //response will contain new book object including atleast _id and title
     })
 
-
-
-
-
-    
-    .delete(function(req, res){
+    .delete(function(req, res) {
       //if successful response will be 'complete delete successful'
     });
 
+  app
+    .route("/api/books/:id")
+    .get(function(req, res) {
+      var bookIdPar = req.params.id;
 
 
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      var bookid = req.params.id;
+      Book.findOne( {
+        _id: bookIdPar
+      })
+        .exec(
+          (err, data) => {
+            if (err) return console.error(err);
+
+            res.json(data)
+
+
+          }
+        )
+
+     
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function(req, res) {
       var bookid = req.body.id;
       var comment = req.body.comment;
-
 
       Book.findOneAndUpdate(
         {
@@ -144,33 +128,28 @@ module.exports = function (app) {
         {
           $push: {
             comments: comment
-            
           }
         }
-      ).exec( (err, data) => {
+      ).exec((err, data) => {
         if (err) return console.error(err);
 
-        if(data) {
-          console.log('Book updated')
-         
+        if (data) {
+          console.log("Book updated");
+
           res.json({
             _id: data._id,
             title: data.title,
             //data is an object return with find, but before update
             comments: [...data.comments, comment]
           });
-
         }
-      })
-
-
+      });
 
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function(req, res) {
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
--- test/func_tests.js
+++ test/func_tests.js
@@ -5,9 +5,13 @@ var server = require('../server');
 
 chai.use(chaiHttp);
 
+
+
+
+
 describe('Routing tests', function() {
 
-
+//get all books
   it('#example Test GET /api/books', function(done){
     chai.request(server)
      .get('/api/books')
@@ -23,7 +27,7 @@ describe('Routing tests', function() {
 
 
 
-
+//post a book
   it("POST /api/books with title => create book object/expect book object", function(done) {
     chai
     .request(server)
@@ -48,6 +52,38 @@ describe('Routing tests', function() {
   })
 })
 
+describe('GET /api/books/[id] => book object with [id]', function(){
+      
+ // it('Test GET /api/books/[id] with id not in db',  function(done){
+   // //done();
+ // });
+  
+  it('Test GET /api/books/[id] with valid id in db',  function(done){
+    chai.request(server)
+    .get('/api/books/Wx7ZA7z39')
+    .end(function(err, res){
+      assert.equal(res.status, 200);
+      assert.equal(res.type, "application/json");
+
+      assert.equal(res.body._id, "Wx7ZA7z39");
+      assert.equal(res.body.title, "test book2");
+      assert.isArray(res.body.comments);
+
+
+
+
+      done();
+    });
+
+
+
+  });
+  
+});
+
+
+
+//'update' post
 describe('POST /api/book/[id] -> add comment/expect book object with id', function() {
 
   it('Test POST /api/books/[id] with comment', function(done) {
