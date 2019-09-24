cf74477dfe1a4e202a5cb3dde9f8a0aad7c97a2d
Achenson <ach.01.mail@gmail.com>
2019-09-24 18:14:58 +0200

    basic static data

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import uuid from 'uuid';
 import './App.css';
+
+
+
+function BooksDisplay( {index, book}) {
+  return(
+    <div className="BooksDisplay">
+      {book._id}<br/>{book.title}{` - ${book.comments.length} comments`}
+    </div>
+  )
+}
+
 
 function App() {
 
-  
+  const [books, setBooks] = useState([
+    {_id: uuid(), title: "New book", comments : ["first comment"]},
+    {_id: uuid(), title: "1984", comments : []},
+    {_id: uuid(), title: "Test Book ", comments : ["one", "two"]},
+    {_id: uuid(), title: " Wikipedia offline", comments : ["test comment"]}
+  ]);
+
+  console.log(setBooks);
+  console.log(books)
+
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Test di</h1>
+      <div className="book-list">
+        {
+          books.map(
+            (book, index) => (
+              <BooksDisplay key={index} book={book}/>
+            )
+          )
+        }
+
+      </div>
     </div>
+  
   );
 }
 
