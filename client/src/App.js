4b4cb661ee75db36f6910350feba9d43cae87054
Achenson <ach.01.mail@gmail.com>
2019-09-28 15:26:58 +0200

    new book & adding comments works

--- client/src/App.js
+++ client/src/App.js
@@ -10,67 +10,82 @@ App ( F dispComents F addBook)  | 2x state -> books, comments to disply (for cli
     Comments (-> comments state) | 1x state
 */
 
-function NewBookForm({addBook}) {
+function NewBookForm({ addBook }) {
+  const [value, setValue] = useState("");
 
-  const [value, setValue] = useState('');
-
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
 
-    if(!value) return;
+    if (!value) return;
     addBook(value);
-    setValue('');
+    setValue("");
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='New Book Title' value={value} onChange = {e => setValue(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="New Book Title"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
         <br></br>
-        <button style={{width: '148px', height: '25px'}}>Submit New Book!</button>
+        <button style={{ width: "148px", height: "25px" }}>
+          Submit New Book!
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
+function Comments({ comments, addComment, deleteBook, books }) {
+  const [value, setValue] = useState("");
 
-function Comments({ comments, addComment, deleteBook, books}) {
-
-  const [value, setValue] = useState('');
-
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
 
-    if(!value) return;
+    if (!value) return;
     addComment(value);
-    setValue('');
+    setValue("");
   }
 
-
-  if(!comments.isHidden) {
+  if (!comments.isHidden) {
     return (
       <div style={{ backgroundColor: "lightgray" }}>
-      <p><b>{books[comments.indexOfDisplayedBook].title}</b>{` (id: ${books[comments.indexOfDisplayedBook]._id})`}</p> 
-      
+        <p>
+          <b>{books[comments.indexOfDisplayedBook].title}</b>
+          {` (id: ${books[comments.indexOfDisplayedBook]._id})`}
+        </p>
+
         <ul>
-          {books[comments.indexOfDisplayedBook].arrOfComments.map((comment, index) => (
-            <li key={index}>{comment}</li>
-          ))}
+          {books[comments.indexOfDisplayedBook].arrOfComments.map(
+            (comment, index) => (
+              <li key={index}>{comment}</li>
+            )
+          )}
         </ul>
-  
+
         <form onSubmit={handleSubmit}>
-          <input type='text' placeholder='New Comment' value={value} onChange = {e => setValue(e.target.value)}/>
+          <input
+            type="text"
+            placeholder="New Comment"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
           <br></br>
-          <button style={{width: '148px', height: '25px'}}>Add Comment</button>
+          <button style={{ width: "148px", height: "25px" }}>
+            Add Comment
+          </button>
         </form>
-        <button style={{width: '148px', height: '25px'}} onClick={deleteBook}>Delete Book</button>
+        <button style={{ width: "148px", height: "25px" }} onClick={deleteBook}>
+          Delete Book
+        </button>
       </div>
     );
   } else {
-    return(null)
+    return null;
   }
-
-
 }
 
 function BooksDisplay({ index, book, dispComments }) {
@@ -82,9 +97,6 @@ function BooksDisplay({ index, book, dispComments }) {
           {` - ${book.arrOfComments.length} comments`}
         </p>
       </div>
-     
-
-
     </div>
   );
 }
@@ -94,14 +106,15 @@ function App() {
     { _id: uuid(), title: "New book", arrOfComments: ["first comment"] },
     { _id: uuid(), title: "1984", arrOfComments: [] },
     { _id: uuid(), title: "Test Book ", arrOfComments: ["one", "two"] },
-    { _id: uuid(), title: " Wikipedia offline", arrOfComments: ["test comment"] }
+    {
+      _id: uuid(),
+      title: " Wikipedia offline",
+      arrOfComments: ["test comment"]
+    }
   ]);
 
   const [comments, setComments] = useState({
     isHidden: true,
-    //bookTitle: "book title",
-    //objectId: "randomObjectId123",
-    //arrOfComments: ["comment1", "comment2"]
     indexOfDisplayedBook: 0
   });
 
@@ -111,76 +124,59 @@ function App() {
   function dispComments(index) {
     const newComments = {
       isHidden: false,
-      //bookTitle: books[index].title,
-      //objectId: books[index]._id,
-      //arrOfComments: [...books[index].comments]
       indexOfDisplayedBook: index
-    }
-   
-    setComments(newComments);
-  };
+    };
 
-  function addBook (value) {
-    const newBooks = [...books, {_id: uuid(), title: value, comments: [] }];
+    setComments(newComments);
+  }
+
+  function addBook(value) {
+    const newBooks = [...books, { _id: uuid(), title: value, arrOfComments: [] }];
     setBooks(newBooks);
   }
 
-  function addComment (value) {
-    
+  function addComment(value) {
     const booksWithNewComments = [...books];
-    //searching for specific book in books state by using comments' state objectId
-    function findBook(el) {
-      return el._id === comments.objectId 
-    }
-    let indexOfBookToChange = booksWithNewComments.findIndex(findBook)
-    
-    booksWithNewComments[indexOfBookToChange].comments.push(value);
-
+    booksWithNewComments[comments.indexOfDisplayedBook].arrOfComments.push(
+      value
+    );
     setBooks(booksWithNewComments);
-
-    const newComments = {
-      isHidden: false,
-      bookTitle: books[indexOfBookToChange].title,
-      objectId: books[indexOfBookToChange]._id,
-      arrOfComments: [...books[indexOfBookToChange].comments]
-    }
-    setComments(newComments);
-
-
   }
 
-  function deleteBook () {
+  function deleteBook() {
     const newBooks = [...books];
 
     function findBookToDelete(el) {
-      return el._id===comments.objectId
+      return el._id === comments.objectId;
     }
 
     let indexOFBookToDelete = newBooks.findIndex(findBookToDelete);
 
-    const newBooks2 = newBooks.splice(indexOFBookToDelete, 1)
+    const newBooks2 = newBooks.splice(indexOFBookToDelete, 1);
 
     setBooks(newBooks2);
-
-
-
-
-
   }
 
   return (
     <div className="App">
       <h1>Test di</h1>
-      <NewBookForm addBook={addBook}/>
+      <NewBookForm addBook={addBook} />
       <div className="book-list">
         {books.map((book, index) => (
-          <BooksDisplay index={index} key={index} book={book} dispComments={dispComments}/>
+          <BooksDisplay
+            index={index}
+            key={index}
+            book={book}
+            dispComments={dispComments}
+          />
         ))}
       </div>
-      <Comments comments={comments} addComment={addComment} deleteBook={deleteBook} books={books} />
-
-
-  
+      <Comments
+        comments={comments}
+        addComment={addComment}
+        deleteBook={deleteBook}
+        books={books}
+      />
     </div>
   );
 }
