0de838cc48906f1fab6ff7217814c49f8e40115f
achenson <45999022+Achenson@users.noreply.github.com>
2019-09-30 15:27:07 +0200

    initial commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+   res.sendFile(process.cwd() + '/views/index.html');
   });
 
 //For FCC testing purposes
