4bb2ccd32af727a7c2ee789c1951b8d75e2f3f99
achenson <45999022+Achenson@users.noreply.github.com>
2019-10-08 13:37:11 +0200

    build folder added

---
+++ client/build/precache-manifest.dd3da74d089d74b8de1930bf91d495d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace31f5382a1bf5c7229947ea173b7db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b63550c5dc4aa6ff71",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a88e08333e440a43b42b",
+    "url": "/static/js/2.eee38eb1.chunk.js"
+  },
+  {
+    "revision": "a3b63550c5dc4aa6ff71",
+    "url": "/static/js/main.35d08775.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  }
+]);
